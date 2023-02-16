bea6dec590697c316f67b22fe761222bb08ab74f
Reodit <dino9525@naver.com>
2023-02-16 17:36:20 +0900

    [Fix] Jenkins Build Setting 변경
    NC-338
    Jenkins Build Setting 변경

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -22,34 +22,37 @@ class BuildScript {
 
 	static void PerformMacOSXBuild ()
 	{
-		string target_dir = APP_NAME;
-		GenericBuild(SCENES, TARGET_DIR + "/Mac" + "/" + target_dir, BuildTarget.StandaloneOSX,BuildOptions.None);
+		string target_path = "/Mac/";
+		string target_exec = APP_NAME;
+		GenericBuild(SCENES, TARGET_DIR + target_path, target_exec, BuildTarget.StandaloneOSX,BuildOptions.None);
 	}
 
 	static void PerformWindowsBuild ()
 	{
-		string target_dir = APP_NAME + ".exe";
-		GenericBuild(SCENES, TARGET_DIR + "/Windows" + target_dir, BuildTarget.StandaloneWindows,BuildOptions.None);
+		string target_path = "/Windows/";
+		string target_exec = APP_NAME + ".exe";
+		GenericBuild(SCENES, TARGET_DIR + target_path, target_exec,  BuildTarget.StandaloneWindows,BuildOptions.None);
 	}
 	
 	static void PerformWebGLBuild ()
 	{
-		string target_dir = "webgl";
-		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.WebGL,BuildOptions.None);
+		string target_dir = "/webgl/";
+		GenericBuild(SCENES, TARGET_DIR + target_dir, "", BuildTarget.WebGL,BuildOptions.None);
 	}
 
 	static void PerformAndroidBuild ()
 	{
-		string target_dir = APP_NAME + ".apk";
-		GenericBuild(SCENES, TARGET_DIR + "/Android" + target_dir, BuildTarget.Android,BuildOptions.None);
+		string target_dir = "/Android/";
+		string target_exec = APP_NAME + ".apk";
+		GenericBuild(SCENES, TARGET_DIR + target_dir, target_exec, BuildTarget.Android,BuildOptions.None);
 	}
 
 	static void PerformiOSBuild ()
 	{
-		string target_dir = "iOS";
+		string target_path = "/IOS/";
 		//We do not build the xcodeproject in the target directory, since we do not want to archive the
 		//entire xcode project, but instead build with XCode, then output the .ipa through Jenkins
-		GenericBuild(SCENES, TARGET_DIR + "/IOS", BuildTarget.iOS,BuildOptions.None);
+		GenericBuild(SCENES, TARGET_DIR + target_path, "",BuildTarget.iOS,BuildOptions.None);
 	}
 
 	private static string[] FindEnabledEditorScenes() {
@@ -61,13 +64,13 @@ class BuildScript {
 		return EditorScenes.ToArray();
 	}
 
-	static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+	static void GenericBuild(string[] scenes, string target_dir, string target_exec,  BuildTarget build_target, BuildOptions build_options)
 	{
 		EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
-		var res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
+		var res = BuildPipeline.BuildPlayer(scenes, target_dir+target_exec, build_target, build_options);
 		if (res.summary.result == BuildResult.Succeeded)
 		{
-          	   ZipDirectory (res.summary.outputPath); 
+          	   ZipDirectory (target_dir); 
         }
 		else {
 			throw new Exception("BuildPlayer failure: " + res);
