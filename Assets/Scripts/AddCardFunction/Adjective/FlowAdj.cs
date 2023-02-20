ea74c4954ba313b1624e0e04abb576933306ab27
MoHoDu <imvagueness@gmail.com>
2023-02-20 18:57:38 +0900

    [Fix] levelEditor bug fix
    #NC-351
    
    1) levelEditor 버그 수정

--- Assets/Scripts/AddCardFunction/Adjective/FlowAdj.cs
+++ Assets/Scripts/AddCardFunction/Adjective/FlowAdj.cs
@@ -2,6 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BlendMode
+{
+    Opaque = 0,
+    Cutout,
+    Fade,
+    Transparent
+}
+
 public class FlowAdj : IAdjective
 {
     private EAdjective adjectiveName = EAdjective.Flow;
@@ -56,6 +64,21 @@ public class FlowAdj : IAdjective
     {
         yield return null;
         obj.gameObject.layer = 4;
+        MeshRenderer[] meshes = obj.transform.GetComponentsInChildren<MeshRenderer>();
+        Debug.Log(meshes.Length);
+        foreach (MeshRenderer m in meshes)
+        {
+            for (int i = 0; i < m.materials.Length; i++)
+            {
+                Debug.Log(m);
+                Material mat = new Material(m.materials[i]);
+                changeRenderMode(mat, BlendMode.Fade);
+                Color color = mat.color;
+                color.a = 0.5f;
+                mat.color = color;
+                m.materials[i] = mat;
+            }
+        }
     }
 
     IEnumerator AbandonFlow(InteractiveObject obj)
@@ -68,5 +91,56 @@ public class FlowAdj : IAdjective
     public IAdjective DeepCopy()
     {
         return new FlowAdj();
+    }
+
+    public static void changeRenderMode(Material standardShaderMaterial, BlendMode blendMode)
+    {
+        switch (blendMode)
+        {
+            case BlendMode.Opaque:
+                standardShaderMaterial.SetFloat("_Mode", 0.0f);
+                standardShaderMaterial.SetOverrideTag("RenderType", "Opaque");
+                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                standardShaderMaterial.SetInt("_ZWrite", 1);
+                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                standardShaderMaterial.renderQueue = -1;
+                break;
+            case BlendMode.Cutout:
+                standardShaderMaterial.SetFloat("_Mode", 1.0f);
+                standardShaderMaterial.SetOverrideTag("RenderType", "Opaque");
+                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                standardShaderMaterial.SetInt("_ZWrite", 1);
+                standardShaderMaterial.EnableKeyword("_ALPHATEST_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                standardShaderMaterial.renderQueue = 2450;
+                break;
+            case BlendMode.Fade:
+                standardShaderMaterial.SetFloat("_Mode", 2.0f);
+                standardShaderMaterial.SetOverrideTag("RenderType", "Transparent");
+                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                standardShaderMaterial.SetInt("_ZWrite", 0);
+                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
+                standardShaderMaterial.EnableKeyword("_ALPHABLEND_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                standardShaderMaterial.renderQueue = 3000;
+                break;
+            case BlendMode.Transparent:
+                standardShaderMaterial.SetFloat("_Mode", 3.0f);
+                standardShaderMaterial.SetOverrideTag("RenderType", "Transparent");
+                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                standardShaderMaterial.SetInt("_ZWrite", 0);
+                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
+                standardShaderMaterial.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                standardShaderMaterial.renderQueue = 3000;
+                break;
+        }
     }
 }
