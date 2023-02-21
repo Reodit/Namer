e3e2887fdd58b8012276ce5fd5cdc9d69d656f59
Reodit <dino9525@naver.com>
2023-02-22 01:45:49 +0900

    [Docs] Doxygen Summary 작성
    Doxygen Summary 작성작업으로, 따로 Jira 표시 하지 않습니다

--- Assets/Scripts/FSM/PlayerEntity.cs
+++ Assets/Scripts/FSM/PlayerEntity.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+
+/// <summary>
+/// 플레이어의 상태 enum값
+/// </summary>
 public enum PlayerStates { Idle = 0, Move, Push, Victory, Teeter, Obtain, Climb, AddCard, EndPoint }
 
+/**
+	* @class PlayerEntity
+	* @brief [BaseEntity를 상속받은 Player의 Entity입니다.]
+*/
 public class PlayerEntity : BaseGameEntity
 {	
 	private	Dictionary<PlayerStates, IState<PlayerEntity>> states;
 	private	StateMachine<PlayerEntity> stateMachine;
+	/// <summary>
+	/// 플레이어 Entity의 현태 상태값
+	/// </summary>
 	public PlayerStates currentStates;
 	#region components
-    private Rigidbody rb;
+    /// <summary>
+    /// 플레이어 Entity의 Animator
+    /// </summary>
     public Animator pAnimator;
 	#endregion
 
@@ -17,15 +30,18 @@ public class PlayerEntity : BaseGameEntity
     {
 		base.Start();
         pAnimator = GetComponentInChildren<Animator>();
-        rb = GetComponent<Rigidbody>();
         FindObjectOfType<StateMachineRunner>().entitys.Add(this);
         GameManager.GetInstance.localPlayerEntity = this;
     }
 
+	/// <summary>
+	/// Entity 초기화
+	/// 1. 새 Dictionary를 생성 후 Dictionary안에 Entity상태를 Key, State 클래스를 Value로 할당해주세요.
+	/// 2. stateMachine을 새로 entity 타입으로 할당한 후 초기값을 세팅해주세요
+	/// </summary>
     public override void Setup()
 	{
 		// states 할당
-		// 새 Dictionary를 생성 후 Dictionary안에 Entity상태를 Key, State 클래스를 Value로 할당해주세요.
 		states = new Dictionary<PlayerStates, IState<PlayerEntity>>();
 		states[PlayerStates.Idle] = new PlayerOwnedStates.IdleState();
 		states[PlayerStates.Move] = new PlayerOwnedStates.MoveState();
@@ -36,7 +52,6 @@ public class PlayerEntity : BaseGameEntity
 		states[PlayerStates.AddCard] = new PlayerOwnedStates.AddCardState();
 		states[PlayerStates.Teeter] = new PlayerOwnedStates.TeeterState();
 		// stateMachine 할당 및 초기화
-		// stateMachine을 새로 entity 타입으로 할당한 후 초기값을 세팅해주세요
 		stateMachine = new StateMachine<PlayerEntity>();
 		stateMachine.Setup(this, states[PlayerStates.Idle]);
 	}
@@ -46,11 +61,18 @@ public class PlayerEntity : BaseGameEntity
 		stateMachine.Execute();
 	}
 
+	/// <summary>
+	/// entity의 상태를 parameter의 상태로 변경합니다
+	/// </summary>
+	/// <param name="newState"></param>
 	public void ChangeState(PlayerStates newState)
 	{
 		stateMachine.ChangeState(states[newState]);
 	}
-
+	
+	/// <summary>
+	/// entity에 저장된 바로 이전 상태로 돌아갑니다.
+	/// </summary>
 	public void RevertToPreviousState()
 	{
 		stateMachine.RevertToPreviousState();
