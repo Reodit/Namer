ebd2891890ae9669ac35e767358cae61c71dbcb9
heokiro <87606800+heokiro@users.noreply.github.com>
2023-02-27 09:43:46 +0900

    [Fix] 꾸밈 부여시 도감 버튼 비활성화
    1. 꾸밈 부여시 도감 버튼 비활성화
    2. 게임 승리 시 카드 다운 실행

--- Assets/Scripts/Scenario/ScenarioController.cs
+++ Assets/Scripts/Scenario/ScenarioController.cs
@@ -520,6 +520,7 @@ public class ScenarioController : MonoBehaviour
     IEnumerator OpenClearPanel()
     {
         yield return new WaitForSeconds(delayWinUI);
+        CardManager.GetInstance.CardsDown();
         stageClearPanel.SetActive(true);
     }
 
